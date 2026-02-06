1f1f0523223d6c4b37ca433c89e70f7b38681ddb
ChuanSWT <chuanswt@gmail.com>
2026-02-06 20:27:14 +0800

    save

--- c++/backend/filetransor.cpp
+++ c++/backend/filetransor.cpp
@@ -9,17 +9,34 @@ QString train_str,QString verify_str,QString test_str)
     train_str(train_str),verify_str(verify_str),test_str(test_str)
 {}
 void FileTransor::Work(){
+    //progress=10;emit Progress(progress);
     //==打开路径------------------------
-    QUrl src_url(src);QString src_path = src_url.toLocalFile();
-    QUrl tg_url(tg);QString tg_path = tg_url.toLocalFile();
-    QDir src_dir(src_path);
-    QDir tg_dir(tg_path);
+    QUrl src_url(src);
+    QUrl tg_url(tg);
+    QDir src_dir(src_url.toLocalFile());
+    QDir tg_dir(tg_url.toLocalFile());
+    //==开始逻辑判断----------------------
+    //检测images文件夹和labels文件夹是否存在
+    if(!src_dir.exists("images")){
+        RaiseERROR("源文件 找不到images子目录");
+        return;
+    }
+    if(!src_dir.exists("labels")){
+        RaiseERROR("源文件 找不到labels子目录");
+        return;
+    }
+    //==扫描文件-------------------------
+    QDir images_dir(src_dir.filePath("images"));
+    QDir labels_dir(src_dir.filePath("labels"));
 
-    QFileInfoList list = src_dir.entryInfoList(
+    QFileInfoList list = images_dir.entryInfoList(
         QDir::Files | QDir::NoDotAndDotDot
         );
-    //progress=100;
-    //emit Progress(100);
+
+    for (const QFileInfo& info : list) {
+        qDebug() << info.fileName()
+        << info.absoluteFilePath();
+    }
     emit RaiseERROR("你还没写完呢");
     emit Finished();
 }
